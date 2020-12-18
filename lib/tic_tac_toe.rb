dccff1329f9a594378a1678261a48be9847378ac
Romano Livi <romano_livi@outlook.com>
2020-12-18 19:39:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -78,6 +78,12 @@ WIN_COMBINATIONS = [
       win_index_1 = single_combo[0]
       win_index_2 = single_combo[1]
       win_index_3 = single_combo[2]
+      
+      position_1 = @board[win_index_1]
+      position_2 = @board[win_index_2]
+      position_3 = @board[win_index_3]
+      
+      if position_1 == position_2 && position_2 == position_3
   
   
   
